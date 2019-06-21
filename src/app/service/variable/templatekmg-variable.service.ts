982acf6e4ef8f674f8213e1eb25bc44ec6bb8ff9
Juan Ignacio <segurajig@gmail.com>
2019-06-21 08:56:41 -0300

    set get name and logo in service templatekmg

--- src/app/service/variable/templatekmg-variable.service.ts
+++ src/app/service/variable/templatekmg-variable.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { TemplateKMG } from '@app/model/templatekmg.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemplatekmgVariableService {
 
-  constructor() { }
+  private template_variables: TemplateKMG;
+
+  constructor() {
+    this.template_variables = {
+      Name: "KMG",
+      Logo: "/image/123.jpg"
+    };
+  }
+
+  public getName(): string{
+    return this.template_variables.Name;
+  }
+
+  public getLogo(): string{
+    return this.template_variables.Logo;
+  }
 }
